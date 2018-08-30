a2989a9c8ffce2b86529b02ff8cc6dfd47bb7adb
Parag Shravagi <shravagi.parag@gmail.com>
2018-08-30 16:41:46 +0530

    Created Book API as RESTful Web services using NodeJS. Implemented GET, PUT, POST, PATCH and DELETE methods

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var db = mongoose.connect('mongodb://localhost/libraryApp');
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var bookRouter = require('./Routes/bookRoutes')(Book);
+app.use('/api/books', bookRouter);
+
+app.get('/', function (req, res) {
+    res.send('Welcome to my API!');
+});
+
+app.listen(port, function () {
+    console.log('Gulp is running my app on port : ' + port);
+});
